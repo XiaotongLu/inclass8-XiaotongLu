c3aeac4655998f6d2200adc68fc3885f4cf40c34
Aryeh <warmflasha@gmail.com>
2017-09-21 12:55:15 -0500

    wrote inclass8

---
+++ inclass8.m
@@ -0,0 +1,12 @@
+%Inclass assignment 8
+
+%Using the swalign function
+% A. Create a random DNA sequence of length 100. 
+% B. Create a second sequence from the first one by changing the middle 20 basepairs (at positions 41-60)
+% C. run swalign on the two sequences with the default parameters
+% D. run swalign with much higher and lower values of the GapOpen parameter
+% and explain the results. 
+% E. run swalign with a low value of GapOpen but a high value of ExtendGap,
+% explain the result
+% F. run swalign align with the default value of GapOpen and a low value
+% of ExtendGap and explain the result. 
